1c7b203891262d40afc4575cd0aae0a7f39fb9a9
Greg <sava8640@mylaurier.ca>
2021-06-03 18:00:21 -0400

    Updated pageLoader

--- pageLoader.js
+++ pageLoader.js
@@ -1,6 +1,6 @@
 //This script loads the modular components of the site via jQuery 
-jQuery(document).ready(function(){
-    jQuery("#siteNav").load("navbar.html #ramblNav");
+$(function(){
+    $("#siteNav").load("navbar.html #ramblNav");
 });
 
 $(window).on('scroll', function(){
