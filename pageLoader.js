b18e1a8b9f940c49af30e70ceb267f8bf91de8f8
Greg <sava8640@mylaurier.ca>
2021-06-06 15:54:18 -0400

    Added a duck

--- pageLoader.js
+++ pageLoader.js
@@ -1,7 +1,10 @@
 //This script loads the modular components of the site via jQuery 
 $(function(){
     $("#siteNav").load("pageElements.html #ramblNav-2");
+    $("#pCard-1").load("pageElements.html #prvwCard");
 });
+
+$("img").attr("src","duck.jpg")
 
 $(window).on('scroll', function(){
     if ($(window).scrollTop() >= 0) {
