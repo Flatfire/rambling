dd1408fdfcc8dd302cfef6172d849ed39d5d7cff
Greg <sava8640@mylaurier.ca>
2021-06-13 16:43:22 -0400

    Half-baked login modal

--- pageLoader.js
+++ pageLoader.js
@@ -1,23 +1,25 @@
-//This script loads the modular components of the site via jQuery 
+//Load modular components of the site via jQuery 
 $(function(){
     $("#siteNav").load("pageElements.html #ramblNav-2");
     $("#pCard-1").load("pageElements.html #prvwCard");
     $("#pCard-2").load("pageElements.html #prvwCard");
     $("#pCard-3").load("pageElements.html #prvwCard");
-    $("#modals").load("pageElements.html #login-modal")
+    //$("#modals").load("pageElements.html #login-modal");
 	
     $('#siteNav').addClass('sticky-top');
 });
 
 $("img").attr("src","duck.jpg")
 
-/*
-$(window).on('scroll', function(){
-    if ($(window).scrollTop() >= 0) {
-        $('#siteNav').addClass('sticky-top');
+/*$(document).on("click", "#rememberLogin", function(){
+    if ($(this).is(':checked')){
+        $(this).siblings('label').fadeOut(function(){
+            $(this).html("You will be remembered").fadeIn();
+        });     
     }
-    else {
-        $('#siteNav').removeClass('sticky-top');
+    else{
+        $(this).siblings('label').fadeOut(function(){
+            $(this).html("Remember Me").fadeIn();
+        });
     }
-});
-*/
+})*/
