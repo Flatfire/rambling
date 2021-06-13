18da660a3ffa7686e3564a676f9f85e0f513c053
Greg <sava8640@mylaurier.ca>
2021-06-13 16:43:45 -0400

    Corrected js for modal load

--- pageLoader.js
+++ pageLoader.js
@@ -4,14 +4,14 @@ $(function(){
     $("#pCard-1").load("pageElements.html #prvwCard");
     $("#pCard-2").load("pageElements.html #prvwCard");
     $("#pCard-3").load("pageElements.html #prvwCard");
-    //$("#modals").load("pageElements.html #login-modal");
+    $("#modals").load("pageElements.html #login-modal");
 	
     $('#siteNav').addClass('sticky-top');
 });
 
 $("img").attr("src","duck.jpg")
 
-/*$(document).on("click", "#rememberLogin", function(){
+$(document).on("click", "#rememberLogin", function(){
     if ($(this).is(':checked')){
         $(this).siblings('label').fadeOut(function(){
             $(this).html("You will be remembered").fadeIn();
@@ -22,4 +22,4 @@ $("img").attr("src","duck.jpg")
             $(this).html("Remember Me").fadeIn();
         });
     }
-})*/
+})
