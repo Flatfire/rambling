19782a3265d79c6db598edaad248ccdb280f1193
Greg <sava8640@mylaurier.ca>
2021-06-13 17:42:33 -0400

    Login/Signup Modal

--- pageLoader.js
+++ pageLoader.js
@@ -1,13 +1,14 @@
 //Load modular components of the site via jQuery 
 $(function(){
-    $("#siteNav").load("pageElements.html #ramblNav-2");
+    $("#siteNav").load("pageElements.html #ramblNav-2", function(){
+        $('#siteNav').addClass('sticky-top');
+    });
     $("#pCard-1").load("pageElements.html #prvwCard");
     $("#pCard-2").load("pageElements.html #prvwCard");
     $("#pCard-3").load("pageElements.html #prvwCard");
-    $("#modals").load("pageElements.html #accountModal");
-    
-
-    $('#siteNav').addClass('sticky-top');
+    $("#modals").load("pageElements.html #accountModal",function(){
+        $("#accountModalDialog").load("pageElements.html #loginModal");
+    }); 
 });
 
 // Uses document selector due to dynamic content load
@@ -24,6 +25,15 @@ $(document).on("click", "#rememberLogin", function(){
     }
 })
 
-$(document).on("click", "#btnOne", function(){
-    $("#accountModalDialog").load("pageElements.html #loginModal");
+$(document).on("click", "#signUp", function(){
+    $("#accountModalDialog").fadeOut(function(){
+        $(this).empty();
+        $(this).load("pageElements.html #signupModal").fadeIn();
+    }); 
+})
+$(document).on("click", "#logIn", function(){
+    $("#accountModalDialog").fadeOut(function(){
+        $(this).empty();
+        $(this).load("pageElements.html #loginModal").fadeIn();
+    }); 
 })
