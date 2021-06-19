2162bf7e89c745d2c60a9b33610f5ba6522899d1
Greg <sava8640@mylaurier.ca>
2021-06-19 16:06:42 -0400

    Form submissions now fire

--- pageLoader.js
+++ pageLoader.js
@@ -1,40 +1,40 @@
 //Load modular components of the site via jQuery 
-$(function(){
-    $("#siteNav").load("pageElements.html #ramblNav-2", function(){
+$(function () {
+    $("#siteNav").load("pageElements.html #ramblNav-2", function () {
         $('#siteNav').addClass('sticky-top');
     });
     $("#pCard-1").load("pageElements.html #prvwCard");
     $("#pCard-2").load("pageElements.html #prvwCard");
     $("#pCard-3").load("pageElements.html #prvwCard");
-    $("#modals").load("pageElements.html #accountModal",function(){
+    $("#modals").load("pageElements.html #accountModal", function () {
         // Contained within function to ensure parent loads
         $("#accountModalDialog").load("pageElements.html #loginModal");
-    }); 
+    });
 });
 
 // Uses document selector due to dynamic content load
-$(document).on("click", "#rememberLogin", function(){
-    if ($(this).is(':checked')){
-        $(this).siblings('label').fadeOut(function(){
+$(document).on("click", "#rememberLogin", function () {
+    if ($(this).is(':checked')) {
+        $(this).siblings('label').fadeOut(function () {
             $(this).html("You will be remembered").fadeIn();
-        });     
+        });
     }
-    else{
-        $(this).siblings('label').fadeOut(function(){
+    else {
+        $(this).siblings('label').fadeOut(function () {
             $(this).html("Remember Me").fadeIn();
         });
     }
 })
 
-$(document).on("click", "#signUp", function(){
-    $("#accountModalDialog").fadeOut(function(){
+$(document).on("click", "#signUp", function () {
+    $("#accountModalDialog").fadeOut(function () {
         $(this).empty();
         $(this).load("pageElements.html #signupModal").fadeIn();
-    }); 
+    });
 })
-$(document).on("click", "#logIn", function(){
-    $("#accountModalDialog").fadeOut(function(){
+$(document).on("click", "#logIn", function () {
+    $("#accountModalDialog").fadeOut(function () {
         $(this).empty();
         $(this).load("pageElements.html #loginModal").fadeIn();
-    }); 
+    });
 })
